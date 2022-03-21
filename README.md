97ea26bd5885e56a39122968f1e7c55037091786
Khorum <soul.reaver.67@gmail.com>
2022-03-20 22:48:18 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Python Toolbox
+
+**Spencer Prentiss, 2021.**
+
+Python admin scripts for general everyday tasks.
---
+++ merge-media.py
@@ -0,0 +1,66 @@
+#region Script Info
+
+#	File name:		merge-media.py
+#	Description:	Python script to combine .ts and .aac files into .mp4 files.
+#					Requires ffmpeg executable to handle the merger.
+
+#	Author:			Spencer Prentiss
+
+#endregion
+
+
+#region Includes
+
+from os import getcwd, system
+from os.path import basename, exists, splitext
+from glob import glob
+
+#endregion
+
+#region Defines
+
+def clear(): system('cls')
+
+#endregion
+
+#region Variables
+
+ffmpeg: str = getcwd() + '\\ffmpeg.exe'		#	Script requires ffmpeg to merge .ts and .aac files, needs to be downloaded separately
+mediaGlob: str = getcwd() + '\**'
+tsExt: str = '.ts'
+aacExt: str = '.aac'
+mp4Ext: str = '.mp4'
+
+#endregion
+
+
+#region Main Function
+
+def Main():
+	'''
+	Main call for script work
+	'''
+	
+	clear()
+	if exists(ffmpeg):
+		files = [f for f in glob(mediaGlob, recursive=True) if f.lower().endswith('.ts')]
+		for f in files:
+			tsFile: str = f
+			fileName: str = basename(f)
+			title: str = splitext(fileName)[0].replace(' - ', ': ').replace('_', '?').replace('--', '/')
+			aacFile: str = f.replace(tsExt, aacExt)
+			mp4File: str = f.replace(tsExt, mp4Ext).replace('--', '-').replace('_', '')
+			options: str = "-i \"{0}\" -i \"{1}\" -metadata title=\"{2}\" -c:v copy -c:a copy \"{3}\"".format(tsFile, aacFile, title, mp4File)
+			if exists(aacFile) and not exists(mp4File):
+				system("{0} {1}".format(ffmpeg, options))
+	else:
+		print('"ffmpeg.exe" missing, script cannot continue')
+
+#endregion
+
+#region Main Call and Exit
+
+Main()
+exit(0)
+
+#endregion
